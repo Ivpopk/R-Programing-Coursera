1e82530708ea8bb61ce09cc5ddcdbf15da7d63c7
Ivpopk <68136368+Ivpopk@users.noreply.github.com>
2022-04-19 01:04:16 -0500

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,66 @@
+### Programing Assignment 2: Lexical Scoping.
+
+################################################################################
+
+## First example
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+# First function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m_inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m_inv <<- NULL
+  }
+  get <- function() x
+  set_m_inv <- function(invertir) m_inv <<- invertir
+  get_m_inv <- function() m_inv
+  list(set = set, get = get,
+       set_m_inv = set_m_inv,
+       get_m_inv = get_m_inv)
+}
+
+
+################################################################################
+
+## Second example
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+# Second function
+
+cacheSolve <- function(x, ...) {
+  m_inv <- x$get_m_inv()
+  if(!is.null(m_inv)) {
+    return(m_inv)
+  }
+  matrizOK <- solve(x$get())
+  x$set_m_inv(matrizOK)
+  matrizOK
+}
+
+
